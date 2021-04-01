0715bfb016239193afb457117d2779f7eb202758
wei li <weili.li@aelf.io>
2021-04-01 19:06:11 +0800

    add offChain transmit test

--- test/offChainAggregator.js
+++ test/offChainAggregator.js
@@ -3,33 +3,53 @@ const testContract = artifacts.require("AccessControlledOffchainAggregator");
 contract('master chef', (accounts) => {
     it('eth util', async () => {
         const testfInstance = await testContract.deployed();
-        //await testfInstance.set
+        let transmitterOne = accounts[0];
+        let payeeOne = accounts[1];
+        let transmitterTwo = accounts[3];
+        let payeeTwo = accounts[4];
 
-        var EthUtil = require('ethereumjs-util');
+        let signerOneAddress = "0x824b3998700F7dcB7100D484c62a7b472B6894B6";
+        let sinnerOnePrivateKey = "7f6965ae260469425ae839f5abc85b504883022140d5f6fc9664a96d480c068d";
 
-        const messageToSign = "hello world";
-        const privateKey = "43f2ee33c522046e80b67e96ceb84a05b60b9434b0ee2e3ae4b1311b9f5dcc46";
+        let signerTwoAddress = "0x90aE559e07f46eebF91bD95DD28889ef60A1E87B";
+        let sinnerTwoPrivateKey = "996e00ecd273f49a96b1af85ee24b6724d8ba3d9957c5bdc5fc16fd1067d542a";
 
-        var msgHash = EthUtil.hashPersonalMessage(new Buffer(messageToSign));
-        var signature = EthUtil.ecsign(msgHash, new Buffer(privateKey, 'hex'));
-        var signatureRPC = EthUtil.toRpcSig(signature.v, signature.r, signature.s)
+        
 
-        console.log(signatureRPC);
-    });
+        let transmiters = [transmitterOne, transmitterTwo];
+        let payees = [payeeOne, payeeTwo];
+        let signers = [signerOneAddress, signerTwoAddress];
 
-    it('eth util', async () => {
-        const testfInstance = await testContract.deployed();
-        //await testfInstance.set
+        let configVersion = 1;
+        let encoded = "0x012df";
 
-        var EthUtil = require('ethereumjs-util');
+        await testfInstance.setPayees(transmiters, payees);
+        await testfInstance.setConfig(signers, transmiters, configVersion, encoded);
+        let config = await testfInstance.latestConfigDetails();
+        //console.log(config.configDigest);
 
-        const messageToSign = "hello world";
-        const privateKey = "43f2ee33c522046e80b67e96ceb84a05b60b9434b0ee2e3ae4b1311b9f5dcc46";
 
-        var msgHash = EthUtil.hashPersonalMessage(new Buffer(messageToSign));
-        var signature = EthUtil.ecsign(msgHash, new Buffer(privateKey, 'hex'));
-        var signatureRPC = EthUtil.toRpcSig(signature.v, signature.r, signature.s)
+        let report = "0x000000000000f6f3ed664fd0e7be332f035ec351acf1000000000000000a030700040a00000000000000000000000000000000000000000000000000000000000a05617364617300000000000000000000000000000000000000000000000000";
+        const messageHash = web3.utils.sha3(report);
+        let signatureOne = web3.eth.accounts.sign(messageHash, sinnerOnePrivateKey);
+        let signatureTwo = web3.eth.accounts.sign(messageHash, sinnerTwoPrivateKey);
+        let rs = [signatureOne.r, signatureTwo.r];
+        let ss = [signatureOne.s, signatureTwo.s];
+        let vs = web3.utils.bytesToHex([signatureOne.v, signatureTwo.v, 0, 0, 0, 0, 0, 0, 0, 0, 0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0]);
+        await testfInstance.transmit(report, rs, ss, vs);
+        //await testInstance.verifySign3(report, signature.r, signature.s, signature.v);
+        
 
-        console.log(signatureRPC);
+
+        // function transmit(
+        //     // NOTE: If these parameters are changed, expectedMsgDataLength and/or
+        //     // TRANSMIT_MSGDATA_CONSTANT_LENGTH_COMPONENT need to be changed accordingly
+        //     bytes calldata _report,
+        //     bytes32[] calldata _rs,
+        //     bytes32[] calldata _ss,
+        //     bytes32 _rawVs // signatures
+        // ) external {
     });
 });
+
+
