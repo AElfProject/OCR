03e4a56d59d659e9485e655dc1a942f3a31f80c7
wei li <weili.li@aelf.io>
2021-04-02 17:06:00 +0800

    modify test

--- test/offChainAggregator.js
+++ test/offChainAggregator.js
@@ -11,10 +11,10 @@ contract('master chef', (accounts) => {
         let payeeTwo = accounts[4];
 
         let signerOneAddress = "0x824b3998700F7dcB7100D484c62a7b472B6894B6";
-        let sinnerOnePrivateKey = "7f6965ae260469425ae839f5abc85b504883022140d5f6fc9664a96d480c068d";
+        let signerOnePrivateKey = "7f6965ae260469425ae839f5abc85b504883022140d5f6fc9664a96d480c068d";
 
         let signerTwoAddress = "0x90aE559e07f46eebF91bD95DD28889ef60A1E87B";
-        let sinnerTwoPrivateKey = "996e00ecd273f49a96b1af85ee24b6724d8ba3d9957c5bdc5fc16fd1067d542a";
+        let signerTwoPrivateKey = "996e00ecd273f49a96b1af85ee24b6724d8ba3d9957c5bdc5fc16fd1067d542a";
 
         let transmiters = [transmitterOne, transmitterTwo];
         let payees = [payeeOne, payeeTwo];
@@ -30,8 +30,12 @@ contract('master chef', (accounts) => {
 
         let report = "0x000000000000f6f3ed664fd0e7be332f035ec351acf1000000000000000a030700040a00000000000000000000000000000000000000000000000000000000000a05617364617300000000000000000000000000000000000000000000000000";
         const messageHash = web3.utils.sha3(report);
-        let signatureOne = web3.eth.accounts.sign(messageHash, sinnerOnePrivateKey);
-        let signatureTwo = web3.eth.accounts.sign(messageHash, sinnerTwoPrivateKey);
+        let signatureOne = web3.eth.accounts.sign(messageHash, signerOnePrivateKey);
+        let signatureTwo = web3.eth.accounts.sign(messageHash, signerTwoPrivateKey);
+
+        console.log(signatureOne.r);
+        console.log(signatureOne.s);
+        console.log(signatureOne.v);
         let rs = [signatureOne.r, signatureTwo.r];
         let ss = [signatureOne.s, signatureTwo.s];
         let vs = web3.utils.bytesToHex([signatureOne.v, signatureTwo.v, 0, 0, 0, 0, 0, 0, 0, 0, 0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0]);
